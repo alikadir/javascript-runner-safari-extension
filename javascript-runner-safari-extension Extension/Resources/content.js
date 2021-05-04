f88e57c19ea7397ea3c03961974a251c8d0cdb10
Ali Kadir Bagcioglu <alikadirbagcioglu@gmail.com>
2021-05-05 00:48:25 +0300

    add udemy

--- javascript-runner-safari-extension Extension/Resources/content.js
+++ javascript-runner-safari-extension Extension/Resources/content.js
@@ -1,7 +1,35 @@
-browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
-    console.log("Received response: ", response);
-});
+/*
+ browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
+ console.log("Received response: ", response);
+ });
+ 
+ browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
+ console.log("Received request: ", request);
+ });
+ */
 
-browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("Received request: ", request);
-});
+console.log('init');
+
+if (location.href.includes('https://cloud.digitalocean.com/login')) {
+    console.log('detect - digitalocean');
+    setInterval(() => {
+        if(document.querySelector('input[id=code]').getAttribute('autocomplete') != 'one-time-code' ) {
+            document.querySelector('input[id=code]').setAttribute('autocomplete', 'one-time-code');
+            document.querySelector('input[id=code]').blur();
+            setTimeout(() => {
+                document.querySelector('input[id=code]').focus();
+            }, 1000);
+            console.log('changed one-time-code');
+        }
+    }, 1000);
+} else if (location.href.includes('https://www.udemy.com/course')) {
+    console.log('detect - udemy');
+    document.addEventListener('contextmenu', e => {
+        e.stopImmediatePropagation();
+    });
+    console.log('activeted PiP');
+} else {
+    console.log("nothing");
+}
+
+
