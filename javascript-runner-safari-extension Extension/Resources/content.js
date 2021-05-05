84b7910c1ac1a168b0593dd86611b37556774bb8
Ali Kadir Bagcioglu <alikadirbagcioglu@gmail.com>
2021-05-05 22:36:50 +0300

    facebook and instagram 2FA

--- javascript-runner-safari-extension Extension/Resources/content.js
+++ javascript-runner-safari-extension Extension/Resources/content.js
@@ -28,6 +28,40 @@ if (location.href.includes('https://cloud.digitalocean.com/login')) {
         e.stopImmediatePropagation();
     });
     console.log('activeted PiP');
+} else if (location.href.includes('https://www.facebook.com/checkpoint')) {
+    console.log('detect - facebook');
+    
+    setInterval(() => {
+        if(document.querySelector('input[id=approvals_code]').getAttribute('autocomplete') != 'one-time-code' )
+        {
+            document.querySelector('input[id=approvals_code]').setAttribute('autocomplete', 'one-time-code');
+            document.querySelector('input[id=approvals_code]').blur();
+            
+            setTimeout(()=>{
+                document.querySelector('input[id=approvals_code]').focus();
+            }, 1000)
+            
+            console.log('changed one-time-code');
+            
+        }
+    }, 1000);
+} else if (location.href.includes('https://www.instagram.com/accounts/login/two_factor')) {
+    console.log('detect - instagram');
+    
+    setInterval(() => {
+        if(document.querySelector('input[name=verificationCode]').getAttribute('autocomplete') != 'one-time-code' )
+        {
+            document.querySelector('input[name=verificationCode]').setAttribute('autocomplete', 'one-time-code');
+            document.querySelector('input[name=verificationCode]').blur();
+            
+            setTimeout(()=>{
+                document.querySelector('input[name=verificationCode]').focus();
+            }, 1000)
+            
+            console.log('changed one-time-code');
+            
+        }
+    }, 1000);
 } else {
     console.log("nothing");
 }
